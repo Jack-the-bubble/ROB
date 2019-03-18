3c1637d7800167d691242a09d17e7948db1bba94
John <ms1b.139@gmail.com>
2019-03-18 16:36:13 +0100

    lab2 init commit

---
+++ LAB2/pdf_indep.m
@@ -0,0 +1,16 @@
+function pdf = pdf_indep(pts, para)
+% Liczy funkcjê gêstoœci prawdopodobieñstwa przy za³o¿eniu, ¿e cechy s¹ niezale¿ne
+% pts zawiera punkty, dla których liczy siê f-cjê gêstoœci (punkt = wiersz, bez etykiety!)
+% para - struktura zawieraj¹ca parametry:
+%	para.mu - wartoœci œrednie cech (wiersz na klasê)
+%	para.sig - odchylenia standardowe cech (wiersz na klasê)
+% pdf - macierz gêstoœci prawdopodobieñstwa
+%	liczba wierszy = liczba próbek w pts
+%	liczba kolumn = liczba klas
+
+	% znam rozmiar wyniku, wiêc go alokujê
+	pdf = zeros(rows(pts), rows(para.mu));
+
+	% tu trzeba policzyæ wartoœæ funkcji gêstoœci
+	% jako iloczyn gêstoœci jednowymiarowych
+end
